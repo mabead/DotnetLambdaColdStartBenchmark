3f31e86fe8689df85d0f0b7f85ae2a10df717abb
Maxime Beaudry <mabead@users.noreply.github.com>
2019-03-01 13:13:33 -0500

    add more logs with timestamps

--- AspnetCoreLambda/LambdaEntryPoint.cs
+++ AspnetCoreLambda/LambdaEntryPoint.cs
@@ -1,33 +1,35 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace AspnetCoreLambda
 {
-    /// <summary>
-    /// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
-    /// actual Lambda function entry point. The Lambda handler field should be set to
-    /// 
-    /// AspnetCoreLambda::AspnetCoreLambda.LambdaEntryPoint::FunctionHandlerAsync
-    /// </summary>
-    public class LambdaEntryPoint :
-        // When using an ELB's Application Load Balancer as the event source change 
-        // the base class to Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
-        Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
     {
-        /// <summary>
-        /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
-        /// needs to be configured in this method using the UseStartup<>() method.
-        /// </summary>
-        /// <param name="builder"></param>
+        static LambdaEntryPoint()
+        {
+            Console.WriteLine($"{DateTime.UtcNow:o} : LambdaEntryPoint static constructor");
+        }
+
+        public LambdaEntryPoint()
+        {
+            Console.WriteLine($"{DateTime.UtcNow:o} : LambdaEntryPoint constructor");
+        }
+
         protected override void Init(IWebHostBuilder builder)
         {
+            Console.WriteLine($"{DateTime.UtcNow:o} : Start of Init");
+
             builder
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging(logging =>
+                {
+                    logging
+                        .ClearProviders()
+                        .AddProvider(new CustomLoggerProvider());
+                });
+
+            Console.WriteLine($"{DateTime.UtcNow:o} : End of Init");
         }
     }
 }
